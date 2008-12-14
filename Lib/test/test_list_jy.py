c5e641c60e131644d118762882fbdb2a2eb3e579
Frank Wierzbicki <fwierzbicki@gmail.com>
2008-12-14 18:29:43 +0000

    Test for http://bugs.jython.org/issue600790 added.  Most likely this bug was fixed by Charlie Groves when java-type-newstyle branch was merged.
    --HG--
    extra : close : 1
    extra : convert_revision : svn%3A79b11f02-f210-0410-99aa-fb189b3d1ac4/jython/tags/Release_2_5beta0%405760

--- Lib/test/test_list_jy.py
+++ Lib/test/test_list_jy.py
@@ -10,6 +10,28 @@ class ListTest(unittest.TestCase):
         self.assertEquals(x, [1, 1, 2, 3, 4, 5],
             "Recursive assignment to list slices failed")
 
+    #From http://bugs.jython.org/issue600790
+    def test_setget_override(self):
+        from java.util import ArrayList
+        from java.lang import String
+
+        class GoofyListMapThing (ArrayList):
+
+            def __init__(self):
+                self.silly = "Nothing"
+
+            def __setitem__(self, key, element):
+                self.silly = "spam"
+
+            def __getitem__(self, key):
+                self.silly = "eggs"
+
+        glmt = GoofyListMapThing()
+        glmt['my-key'] = String('el1')
+        self.assertEquals(glmt.silly, "spam")
+        glmt['my-key']
+        self.assertEquals(glmt.silly, "eggs")
+
 def test_main():
     test.test_support.run_unittest(ListTest)
 
