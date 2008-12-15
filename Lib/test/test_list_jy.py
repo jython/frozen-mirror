16550c1ca6dd1066c6166fca5e2e433fd44a236a
Frank Wierzbicki <fwierzbicki@gmail.com>
2008-12-15 14:01:24 +0000

    Reverting test_list_jy.py since tags should almost never be updated.
    --HG--
    extra : close : 1
    extra : convert_revision : svn%3A79b11f02-f210-0410-99aa-fb189b3d1ac4/jython/tags/Release_2_5beta0%405764

--- Lib/test/test_list_jy.py
+++ Lib/test/test_list_jy.py
@@ -3,37 +3,15 @@ import test.test_support
 
 class ListTest(unittest.TestCase):
         
-    def test_recursive_list_slices(self):
-        x = [1,2,3,4,5]
-        x[1:] = x
+        def test_recursive_list_slices(self):
+                x = [1,2,3,4,5]
+                x[1:] = x
 
-        self.assertEquals(x, [1, 1, 2, 3, 4, 5],
-            "Recursive assignment to list slices failed")
-
-    #From http://bugs.jython.org/issue600790
-    def test_setget_override(self):
-        from java.util import ArrayList
-        from java.lang import String
-
-        class GoofyListMapThing (ArrayList):
-
-            def __init__(self):
-                self.silly = "Nothing"
-
-            def __setitem__(self, key, element):
-                self.silly = "spam"
-
-            def __getitem__(self, key):
-                self.silly = "eggs"
-
-        glmt = GoofyListMapThing()
-        glmt['my-key'] = String('el1')
-        self.assertEquals(glmt.silly, "spam")
-        glmt['my-key']
-        self.assertEquals(glmt.silly, "eggs")
+                self.assertEquals(x, [1, 1, 2, 3, 4, 5],
+                        "Recursive assignment to list slices failed")
 
 def test_main():
-    test.test_support.run_unittest(ListTest)
+        test.test_support.run_unittest(ListTest)
 
 if __name__ == "__main__":
-    test_main()
+        test_main()
